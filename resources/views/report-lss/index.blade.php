07d14ae54119037717de62f2e4de2eb167f9f0ea
mutiara-azzahra <my26recovery@gmail.com>
2024-03-21 19:48:27 +0800

    report lss

--- resources/views/report-lss/index.blade.php
+++ resources/views/report-lss/index.blade.php
@@ -79,7 +79,7 @@
   let dateDropdown = document.getElementById('date-dropdown'); 
        
   let currentYear = new Date().getFullYear();    
-  let earliestYear = 2022;     
+  let earliestYear = 2023;     
   while (currentYear >= earliestYear) {      
     let dateOption = document.createElement('option');          
     dateOption.text = currentYear;      
