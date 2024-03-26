ee230710a73945a0dd0a5d4a73c09cc801de2d5b
Mutiara <my26recovery@gmail.com>
2024-03-26 10:16:41 +0800

    master toko

--- resources/views/master-toko/index.blade.php
+++ resources/views/master-toko/index.blade.php
@@ -27,10 +27,11 @@
     <div class="card" style="padding: 10px;">
         <div class="card-body">
             <div class="col-lg-12">  
-                <table class="table table-hover table-bordered table-sm bg-light table-striped" id="example2">
+                <table class="table table-hover table-bordered table-sm bg-light table-striped" id="example1">
                     <thead>
                         <tr style="background-color: #6082B6; color:white">
-                            <th class="text-center">Kode / Nama Toko</th>
+                            <th class="text-center">Kode Toko</th>
+                            <th class="text-center">Nama Toko</th>
                             <th class="text-center">Provinsi</th>
                             <th class="text-center">Kabupaten/Kota</th>
                             <th class="text-center">Telpon</th>
@@ -42,14 +43,13 @@
                         
                         @foreach($list_toko as $p)
                         <tr>
-                            <td class="text-left">[{{ $p->kd_outlet }}] {{ $p->nm_outlet }}</td>
-
+                            <td class="text-center">{{ $p->kd_outlet }}</td>
+                            <td class="text-left">{{ $p->nm_outlet }}</td>
                             @if( $p->kode_prp == 6300)
                             <td class="text-left">Kalimantan Selatan</td>
                             @elseif( $p->kode_prp == 6200)
                             <td class="text-left">Kalimantan Tengah</td>
                             @endif
-
                             @if(isset($p->area_outlet->nm_area))
                             <td class="text-left">{{ $p->area_outlet->nm_area }}</td>
                             @else
@@ -61,8 +61,8 @@
                             <td class="text-center">
                                 <form action="{{ route('master-toko.nonaktif', $p->kd_outlet) }}" method="POST" id="form_nonaktif_{{ $p->kd_outlet }}" data-id="{{ $p->kd_outlet }}">
                                     
-                                    <a class="btn btn-info btn-sm" href="{{ route('master-toko.view', $p->kd_outlet) }}" target="_blank"><i class="fas fa-eye"></i></a>
-                                    <a class="btn btn-warning btn-sm" href="{{ route('master-toko.edit', $p->kd_outlet) }}" target="_blank"><i class="fas fa-edit"></i></a>
+                                    <a class="btn btn-info btn-sm" href="{{ route('master-toko.view', $p->kd_outlet) }}"><i class="fas fa-eye"></i></a>
+                                    <a class="btn btn-warning btn-sm" href="{{ route('master-toko.edit', $p->kd_outlet) }}"><i class="fas fa-edit"></i></a>
 
                                     @csrf
                                     @method('GET')
--- resources/views/master-toko/show.blade.php
+++ resources/views/master-toko/show.blade.php
@@ -23,10 +23,63 @@
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Kota</strong><br>
-                   <br>
+                    <strong>Nama Outlet</strong><br>
+                    {{ $outlet->nm_outlet }}<br>
                 </div>
             </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nama Pemilik</strong><br>
+                    {{ $outlet->nm_pemilik }}<br>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Alamat Toko</strong><br>
+                    {{ $outlet->almt_outlet }}<br>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Alamat Pengirim</strong><br>
+                    {{ $outlet->almt_pengiriman }}<br>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Area Group</strong><br>
+                    {{ $outlet->area_group_2w }}<br>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Telpon</strong><br>
+                    {{ $outlet->tlpn }}<br>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>NPWP</strong><br>
+                    {{ $outlet->npwp }}<br>
+                </div>
+            </div>
+
+            @if($outlet->jth_tempo == 0 ||$outlet->jth_tempo == null)
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Jatuh Tempo</strong> <a style="color:red;"> *wajib diisi</a><br>
+                    Kosong, Harap diisi <a style="color:#0096FF;" href="{{ route('master-toko.edit', $outlet->kd_outlet) }}">disini.</a><br>
+                </div>
+            </div>
+            @else
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Tanggal Jatuh Tempo</strong> *wajib diisi<br>
+                    {{ $outlet->jth_tempo }}<br>
+                </div>
+            </div>
+            @endif
+            
         </div>
     </div>
     
