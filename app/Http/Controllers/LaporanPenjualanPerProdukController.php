129f4723608059f83c88173e4057ee9b86e2f7e0
Mutiara <my26recovery@gmail.com>
2024-04-01 11:00:02 +0800

    laporan penjualan per produk

--- app/Http/Controllers/LaporanPenjualanPerProdukController.php
+++ app/Http/Controllers/LaporanPenjualanPerProdukController.php
@@ -24,37 +24,54 @@ class LaporanPenjualanPerProdukController extends Controller
         $invoices = TransaksiInvoiceHeader::whereBetween('created_at', [$tanggal_awal, $tanggal_akhir])
         ->get();
 
-        $getAmountIchidai = 0;
-        $getAmountBrio    = 0;
-        $getAmountLiquid  = 0;
+        if($produk == 1){
 
-        //ICHIDAI
-        foreach ($invoices as $i) {
-            $getAmountIchidai += $i->details_invoice()->whereIn('part_no', function ($query) {
-                $query->select('part_no')
-                    ->from('master_part')
-                    ->where('level_2', 'IC2');
-                })->sum('nominal_total');
-        }
+            $getAmount = 0;
+
+            //ICHIDAI
+            foreach ($invoices as $i) {
+                $getAmount += $i->details_invoice()->whereIn('part_no', function ($query) {
+                    $query->select('part_no')
+                        ->from('master_part')
+                        ->where('level_2', 'IC2');
+                    })->sum('nominal_total');
+            }
+
+        } elseif($produk == 2) {
+
+            $getAmount    = 0;
         
-        //BRIO
-        foreach ($invoices as $i) {
-            $getAmountBrio += $i->details_invoice()->whereIn('part_no', function ($query) {
-                $query->select('part_no')
-                    ->from('master_part')
-                    ->where('level_2', 'BP2');
-                })->sum('nominal_total');
+            //BRIO
+            foreach ($invoices as $i) {
+                $getAmount += $i->details_invoice()->whereIn('part_no', function ($query) {
+                    $query->select('part_no')
+                        ->from('master_part')
+                        ->where('level_2', 'BP2');
+                    })->sum('nominal_total');
+            }
+        } elseif($produk == 3){
+
+            $getAmount  = 0;
+        
+            //LIQUID
+            foreach ($invoices as $i) {
+                $getAmount += $i->details_invoice()->whereIn('part_no', function ($query) {
+                    $query->select('part_no')
+                        ->from('master_part')
+                        ->where('level_2', 'LQ2');
+                    })->sum('nominal_total');
+            }
+        } elseif($produk == 4){
+
+            $getAmount  = 0;
+        
+            //LIQUID
+            foreach ($invoices as $i) {
+                $getAmount += $i->details_invoice()->sum('nominal_total');
+            }
+
         }
 
-        //LIQUID
-        foreach ($invoices as $i) {
-            $getAmountLiquid += $i->details_invoice()->whereIn('part_no', function ($query) {
-                $query->select('part_no')
-                    ->from('master_part')
-                    ->where('level_2', 'LQ2');
-                })->sum('nominal_total');
-        }
-
-        return view('laporan-penjualan-produk.view', compact('getAmountIchidai','getAmountBrio', 'getAmountLiquid', 'tanggal_awal', 'tanggal_akhir'));
+        return view('laporan-penjualan-produk.view', compact('getAmount', 'tanggal_awal', 'tanggal_akhir'));
     }
 }
